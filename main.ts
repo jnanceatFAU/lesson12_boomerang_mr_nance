b9f9b0bf883de817e11a4d4839f1522e5f976e11
jnanceatFAU <79646242+jnanceatFAU@users.noreply.github.com>
2021-05-13 09:14:10 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,43 @@
-let Boomerang = sprites.create(img`
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    for (let index = 0; index < 10; index++) {
+        Boomerang.x += 5
+        pause(200)
+        Boomerang.image.flipX()
+    }
+    for (let index = 0; index < 10; index++) {
+        Boomerang.x += -5
+        pause(200)
+        Boomerang.image.flipX()
+    }
+})
+let Boomerang: Sprite = null
+let mySprite = sprites.create(img`
+    ........................
+    ........................
+    ...........ccc..........
+    ...........cccc.........
+    .......ccc..ccccccc.....
+    .......cccccc555555cc...
+    ........ccb5555555555c..
+    .....cc..b555555555555c.
+    .....cccb555555ff155555c
+    ......cb55555555ff55d55c
+    ......b5555555555555555c
+    ...cc.b555dd5555bb13bbc.
+    ...cccd55ddddd555b3335c.
+    .....bdddddddddd55b335c.
+    ..cccdddddb55bbddd5555c.
+    ..cccdddddb555bbbbcccc..
+    ...ccddddddb5555cbcdc...
+    ccccbdddddd5cb55cbcc....
+    cddddddddd5555ccbbc.....
+    .cddddddbdd555bbbcc.....
+    ..ccdddbbbdd55cbcdc.....
+    ....ccbbcbddddccdddcc...
+    ......cccdd555dcccccc...
+    ........cccccccc........
+    `, SpriteKind.Player)
+Boomerang = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -16,13 +55,3 @@ let Boomerang = sprites.create(img`
     . . . . 4 4 e . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-for (let index = 0; index < 10; index++) {
-    Boomerang.x += 5
-    pause(200)
-    Boomerang.image.flipX()
-}
-for (let index = 0; index < 10; index++) {
-    Boomerang.x += -5
-    pause(200)
-    Boomerang.image.flipX()
-}
